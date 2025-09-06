6ae1b6eebfff8fb435cbf93dacbc0709823b65a9
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-09-06 12:27:09 +1000

    update for testing

--- testing/src/test/java/com/camundatest/EmailProcessTest.java
+++ testing/src/test/java/com/camundatest/EmailProcessTest.java
@@ -41,7 +41,7 @@ public class EmailProcessTest {
                     .addResourceFromClasspath(bpmnResource)
                     .send()
                     .join();
-        System.out.println("Testiiiing  test");
+        System.out.println("Testiiiing  test again");
             // Assert deployment was successful
             assertNotNull(deploymentEvent, "Deployment event should not be null");
             assertFalse(deploymentEvent.getProcesses().isEmpty(), "At least one process should be deployed");
